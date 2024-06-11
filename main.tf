41f33ed5b3e39033aa52380b45e4fed78c01c7b5
julie <nungujulie@gmail.com>
2024-06-11 12:37:37 -0400

    new commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_group" "developers1" {
+  name = "developers"
+
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer1"
+}
